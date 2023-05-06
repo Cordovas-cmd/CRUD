36ee7877d2764503b48e8b96903ccba09e3c5e99
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-05-06 07:30:32 -0500

    just starting up the server file

---
+++ server.js
@@ -0,0 +1,12 @@
+console.log('Node Started');
+
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+
+app.listen(port, function () {
+    console.log("Listening on Port 3000");
+});
