e86526615a6168f6d3de9fa96b3c2d52d4f5a2ac
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-09-14 20:46:44 -0500

    trying out diff codebase for server

--- server.js
+++ server.js
@@ -1,3 +1,58 @@
+// console.log('Node Started');
+
+// const express = require('express');
+
+// const app = express();
+
+// // a package that serves as MIDDLEWARE that allows me to parse the body of my request fields.
+// const bodyParser = require('body-parser')
+
+// const port = 3000;
+
+
+// // https://www.npmjs.com/package/body-parser Body parser = middleware
+// // allows express to allow body parser to extract data from the form and add it to the body property in a request object
+// // urlencoded = built in method from body parser extract data from the form and add it to the body propert in a request object. 
+
+// // installed mongodb dependancy so we can use this require.
+// const MongoClient = require('mongodb').MongoClient
+
+// const url = 'mongodb://localhost:27017'
+
+// MongoClient.connect(url, (error, client) => {
+//     if (error) return console.error(error);
+
+// console.log('Connected to MongoDB');
+
+// const db = client.db('HelloCrudDatabase');
+
+// const coursesCollection = db.collection('courses');
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.listen(port, function () {
+//     console.log('Listening on Port 3000');
+// });
+
+// // Get request for homepage
+// app.get('/', (req,res) => {
+
+// res.sendFile(__dirname + '/index.html');
+// });
+
+// // Post request for form submit
+// app.post('/courses', (req,res) => {
+
+//     coursesCollection.insertOne(req.body)
+//     .then(result => {
+//         console.log(result)
+//     })
+//     .catch(error => console.error(error));
+// });
+// });
+
+
+
 console.log('Node Started');
 
 const express = require('express');
@@ -9,7 +64,25 @@ const bodyParser = require('body-parser')
 
 const port = 3000;
 
+
+// https://www.npmjs.com/package/body-parser Body parser = middleware
 // allows express to allow body parser to extract data from the form and add it to the body property in a request object
+// urlencoded = built in method from body parser extract data from the form and add it to the body propert in a request object. 
+
+// installed mongodb dependancy so we can use this require.
+const MongoClient = require('mongodb').MongoClient
+
+const url = 'mongodb://localhost:27017'
+
+MongoClient.connect(url, (error, client) => {
+if (error) return console.error(error);
+
+console.log('Connected to MongoDB');
+
+const db = client.db('HelloCrudDatabase');
+
+const coursesCollection = db.collection('courses');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, function () {
@@ -18,25 +91,17 @@ app.listen(port, function () {
 
 // Get request for homepage
 app.get("/", (req, res) => {
+
 res.sendFile(__dirname + '/index.html');
 });
 
 // Post request for form submit
 app.post('/courses', (req,res) => {
-    console.log(req.body)
-})
 
-// installed mongodb dependancy so we can use this require.
-const MongoClient = require('mongodb').MongoClient
-
-const url= 'mongodb://localhost:27017'
-
-MongoClient.connect(url, (error, client) => {
-if (error) {
-    return console.error(error);
-}
-console.log('Connected to MongoDB');
-const db = client.db('HelloCrudDatabase');
-
-
+    coursesCollection.insertOne(req.body)
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error => console.error(error));
+});
 });
